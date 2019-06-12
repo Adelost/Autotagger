f457f93ef3833c4e47f57ef2316675fd6d88ef8e
Mattias Andersson <mattias.andersson@consid.se>
2019-06-12 18:04:57 +0200

    WIP

--- scripts/run-stagger.sh
+++ scripts/run-stagger.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-files=../inputs/swe/txt/*.txt
-java -Xmx2G -jar ../vendor/stagger/stagger.jar -modelfile ../vendor/stagger/models/swedish.bin -tag $files
-mv $files ../inputs/swe/txt-out
-mv ../inputs/swe/txt/*.conll ../inputs/swe/conll
+files=($(echo ../inputs/swe/txt/*.txt))
+
+if test -f ${files[0]}
+then
+  start=$(date +%s.%N)
+  echo "Converting: ${files[*]}"
+  java -Xmx2G -jar ../vendor/stagger/stagger.jar -modelfile ../assets/big/swedish.bin -tag ${files[*]}
+  echo "Done"
+  echo "Moving generated files to output directories"
+  mv ${files[*]} ../inputs/swe/txt-out
+  echo "Done"
+  mv ../inputs/swe/txt/*.conll ../inputs/swe/conll
+  end=$(date +%s.%N)
+  diff=$(echo "$end - $start" | bc)
+  echo "Time to execute: $diff"
+else
+  echo "No files to convert"
+fi
